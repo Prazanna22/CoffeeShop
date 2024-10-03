0c16182a57b1a7c0acf7e9b195a30cdcc0c79870
Durga Prasanna <prasanna.guru02@gmail.com>
2024-10-03 11:18:41 +0530

    Coffee Shop website

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import food from '../assets/all/Food.jpg'
-import bestseller from '../assets/all/BestSeller.jpg'
+import java from '../assets/all/java.jpg'
 import readytoeat from '../assets/all/ReadyToEat.jpg'
 import drinks from '../assets/all/Drinks.jpg'
 import { useNavigate } from 'react-router-dom'
@@ -14,7 +14,7 @@ export const ListItems = () => {
                 <h1 className='font-bold text-[#a7782b] text-3xl text-center my-10'>Handcrafted Curations</h1>
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
                     <div className="flex flex-col justify-center items-center">
-                        <img src={bestseller} alt="" className='w-32 my-5 rounded-full ' onClick={() => { navigate('/menu') }} />
+                        <img src={java} alt="" className='w-32 my-5 rounded-full ' onClick={() => { navigate('/menu') }} />
                         <h1 className='text-black font-medium'>BestSeller</h1>
                     </div>
                     <div className="flex flex-col justify-center items-center">
